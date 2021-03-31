e5ea2f52b4f3226244519928a2bc91f29a633b5d
RubénEu <garcia.ruben@uma.es>
2021-03-31 17:06:42 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple-object-tracker',
-    version='0.0.1',
+    version='0.1.0',
     description='Object tracker with different models and parameters.',
     author='Rubén García Rojas',
     author_email='garcia.ruben@outlook.es',
