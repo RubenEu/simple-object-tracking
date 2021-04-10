a2d350a94b2765cc4db9353680df039b1d71495a
RubénEu <garcia.ruben@uma.es>
2021-04-10 16:51:51 +0200

    Fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='simple-object-tracker',
+    name='simple-object-tracking',
     version='0.1.1',
     description='Object tracker with different models and parameters.',
     author='Rubén García Rojas',
