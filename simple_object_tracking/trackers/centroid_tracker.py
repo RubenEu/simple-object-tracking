8255a974c9fcce0405c775e31c69421266d56423
RubenEu <garcia.ruben@outlook.es>
2021-05-01 00:16:18 +0200

    Arreglado fallo de variable y PEP8

--- simple_object_tracking/trackers/centroid_tracker.py
+++ simple_object_tracking/trackers/centroid_tracker.py
@@ -26,7 +26,7 @@ class CentroidTracker(ObjectTracker):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.max_distance_allowed = 0.10 * max(self.frame_width, self.frame_height)
+        self.max_distance_allowed = 0.10 * max(self.sequence.width, self.sequence.height)
 
     def _matching_step(self,
                        objs_registered: List[Tuple[int, int, Object]],
@@ -37,24 +37,29 @@ class CentroidTracker(ObjectTracker):
         # Si no hay ningún objeto registrado, saltar fase de emparejeamiento para el frame actual.
         if len(objs_registered) == 0:
             return
-        # Si no se ha detectado ningún objeto en el frame actual, tampoco se puede realizar emparejamiento.
+        # Si no se ha detectado ningún objeto en el frame actual, tampoco se puede realizar
+        # emparejamiento.
         if len(objs_actual) == 0:
             return
-        # Para cada uno de los objetos actuales, buscar con cuál de los objetos registrados emparejar.
+        # Para cada uno de los objetos actuales, buscar con cuál de los objetos registrados
+        # emparejar.
         for obj_actual_id, obj_actual_detection in enumerate(objs_actual):
             distances_to_objs_actual = list()
             # Calcular las distancias de los objetos registrados al objeto actual detectado.
             for obj_registered_id, obj_registered in enumerate(objs_registered):
-                obj_registered_uid, obj_registered_last_frame_seen, obj_registered_detected = obj_registered
-                distance = calculate_euclidean_distance(obj_registered_detected.center, obj_actual_detection.center)
+                (obj_registered_uid, obj_registered_last_frame_seen,
+                 obj_registered_detected) = obj_registered
+                distance = calculate_euclidean_distance(obj_registered_detected.center,
+                                                        obj_actual_detection.center)
                 distances_to_objs_actual.append(distance)
             # Busca la distancia mínima.
             min_distance = min(distances_to_objs_actual)
             min_distance_index = distances_to_objs_actual.index(min_distance)
-            # Comprueba que no se ha realizado el emparejamiento del objeto registrado con alguno de los actuales.
+            # Comprueba que no se ha realizado el emparejamiento del objeto registrado con alguno de
+            # los actuales.
             not_matched = min_distance_index not in objs_registered_uids_matched
-            # TODO: Si no se puede realizar el emparejamiento con el primero que está a menos distancia,
-            #  se podría probar con los siguientes...
+            # TODO: Si no se puede realizar el emparejamiento con el primero que está a menos
+            #  distancia, se podría probar con los siguientes...
             if not_matched and min_distance <= self.max_distance_allowed:
                 # Hacer el emparejamiento.
                 self.objects.update_object(
@@ -75,7 +80,8 @@ class CentroidTracker(ObjectTracker):
             # Objetos registrados hasta el momento.
             objs_registered = self.objects.objects()
             objs_registered_uids_matched = list()
-            # Objetos detectados en el frame actual, candidatos para emparejar con alguno de los registrados.
+            # Objetos detectados en el frame actual, candidatos para emparejar con alguno de los
+            # registrados.
             objs_actual = self.objects_in_frame(frame_actual)
             objs_actual_ids_matched = list()
             self._matching_step(objs_registered, objs_registered_uids_matched,
