0303b008f0d60a73c9d4378cd955c27bca19d4e8
RubenEu <garcia.ruben@outlook.es>
2021-05-17 17:26:27 +0200

    Eliminadas las utilidades de video de este paquete

--- simple_object_tracking/utils/__init__.py
+++ simple_object_tracking/utils/__init__.py
@@ -1,2 +1,3 @@
 from simple_object_tracking.utils.maths import euclidean_norm
-from simple_object_tracking.utils.video import TrackingVideo, TrackingVideoProperty
+from simple_object_tracking.utils.geometry import (find_closest_position_to_line,
+                                                   point_distance_to_line)
